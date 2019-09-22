3ed58ba77bb6b9aa50b1a5db29664c7c1da57184
zooeydotmango <newzoe@qq.com>
2019-09-23 03:00:18 +0800

    button.vue

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from 'Vue'
+import Button from './button'
+
+Vue.component('g-button',Button)
+new Vue({
+    el:'#app',
+})
---
+++ src/button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+<scrit>
+    export default{
+    }
+</scrit>
+<style lang="scss">
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border: 1px solid var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
