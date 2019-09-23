b8d1f990035f0d85a88520a2211e3d330fdc16d0
zooeydotmango <newzoe@qq.com>
2019-09-23 19:12:10 +0800

    使用chai进行五个单元测试，第五个需要mock，暂时没写

--- src/app.js
+++ src/app.js
@@ -14,3 +14,69 @@ new Vue({
         loading3:false
     }
 })
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement);
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading:true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement);
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition:'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let order = window.getComputedStyle(svg).order
+    expect(order).to.eq(`2`)
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const gButton = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    gButton.$mount()
+    gButton.$on('click',function () {
+        console.log(1);
+    })
+    let button = gButton.$el
+    button.click()
+    gButton.$el.remove()
+    gButton.$destroy()
+}
