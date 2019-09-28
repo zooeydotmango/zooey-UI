d1783d98d13f6f89252c2d6884b551f2ba673759
zooeydotmango <newzoe@qq.com>
2019-09-28 23:48:40 +0800

    默认phone样式

--- src/col.vue
+++ src/col.vue
@@ -4,52 +4,123 @@
     </div>
 </template>
 <script>
+    let validator = (value) => {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach(key => {
+            if (!['span', 'offset'].includes(key)) {
+                valid = false
+            }
+        })
+        return valid
+    }
     export default {
         name: 'zooeyCol',
         props: {
             span: {
-                type:[Number,String]
+                type: [Number, String]
             },
-            offset:{
-                type:[Number,String]
+            offset: {
+                type: [Number, String]
+            },
+            pad: {type: Object, validator,},
+            narrowPc: {type: Object, validator,},
+            pc: {type: Object, validator,},
+            widePc: {type: Object, validator,}
+        },
+        data() {
+            return {
+                gutter: 0
             }
         },
-        data(){
-            return{
-                gutter:0
-            }
-        },
-        computed:{
-            colStyle(){
+        computed: {
+            colStyle() {
                 return {
-                    paddingLeft: this.gutter/2+'px',
-                    paddingRight: this.gutter/2+'px'
+                    paddingLeft: this.gutter / 2 + 'px',
+                    paddingRight: this.gutter / 2 + 'px'
                 }
             },
-            colClass(){
-                let {span,offset}=this
+            colClass() {
+                let {span, offset, pad, narrowPc, pc, widePc} = this
                 return [
                     span && `col-${span}`,
-                    offset &&`offset-${offset}`
+                    offset && `offset-${offset}`,
+                    ...(pad && [`col-pad-${pad.span}`]),
+                    ...(narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
+                    ...(pc && [`col-pc-${pc.span}`]),
+                    ...(widePc && [`col-wide-pc-${widePc.span}`]),
                 ]
             }
         }
     }
 </script>
 <style lang="scss" scoped>
-    .col{
-        margin: 0 ;
+    .col {
         $class-col: col-;
         $class-offset: offset-;
         @for $n from 1 through 24 {
-            &.#{$class-col}#{$n}{
+            &.#{$class-col}#{$n} {
                 width: ($n / 24)*100%;
             }
         }
         @for $n from 1 through 24 {
-            &.#{$class-offset}#{$n}{
+            &.#{$class-offset}#{$n} {
                 margin-left: ($n / 24)*100%;
             }
         }
     }
+    
+
+    @media (min-width: 577px) and (max-width: 768px) {
+        .col {
+            $class-col: col-pad-;
+            $class-offset: offset-pad-;
+            @for $n from 1 through 24 {
+                &.#{$class-col}#{$n} {
+                    width: ($n / 24)*100%;
+                }
+            }
+            @for $n from 1 through 24 {
+                &.#{$class-offset}#{$n} {
+                    margin-left: ($n / 24)*100%;
+                }
+            }
+        }
+    }
+
+    @media (min-width: 769px) and (max-width: 992px) {
+        .col {
+            $class-col: col-narrow-pc-;
+            $class-offset: offset-narrow-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class-col}#{$n} {
+                    width: ($n / 24)*100%;
+                }
+            }
+            @for $n from 1 through 24 {
+                &.#{$class-offset}#{$n} {
+                    margin-left: ($n / 24)*100%;
+                }
+            }
+        }
+    }
+
+    @media (min-width: 1201px) {
+        .col {
+            $class-col: col-wide-pc-;
+            $class-offset: offset-wide-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class-col}#{$n} {
+                    width: ($n / 24)*100%;
+                }
+            }
+            @for $n from 1 through 24 {
+                &.#{$class-offset}#{$n} {
+                    margin-left: ($n / 24)*100%;
+                }
+            }
+        }
+    }
+
+
 </style>
