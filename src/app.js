b1a6ec75e906f586da3ce05bfa6b92057cba6aa8
zooeydotmango <newzoe@qq.com>
2019-09-29 03:35:40 +0800

    基本功能完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Content from './content'
+import Header from './header'
+import Footer from './footer'
+import Layout from './layout'
+import Sider from './sider'
 
 
 Vue.component('g-button',Button);
@@ -13,6 +18,11 @@ Vue.component('g-button-group',ButtonGroup);
 Vue.component('g-input', Input)
 Vue.component('g-row',Row)
 Vue.component('g-col',Col)
+Vue.component('g-layout',Layout)
+Vue.component('g-content',Content)
+Vue.component('g-header',Header)
+Vue.component('g-footer',Footer)
+Vue.component('g-sider',Sider)
 
 new Vue({
     el:'#app',
---
+++ src/footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'zooeyFooter'
+    }
+</script>
+<style lang="scss" scoped>
+    .footer{
+
+    }
+</style>
