d1525a9b4688b208be2d88c3848cea160e13cc28
zooeydotmango <newzoe@qq.com>
2019-10-10 00:45:15 +0800

    添加tabs相关组件，添加基本 props

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ import Layout from './layout'
 import Sider from './sider'
 import Toast from './toast'
 import Plugin from './plugin'
+import Tabs from './tabs'
+import TabsHead from './tabs-head'
+import TabsBody from './tabs-body'
+import TabsItem from './tabs-item'
+import TabsPane from './tabs-pane'
+
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
@@ -25,6 +31,11 @@ Vue.component('g-header', Header)
 Vue.component('g-footer', Footer)
 Vue.component('g-sider', Sider)
 Vue.component('g-toast', Toast)
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-pane', TabsPane)
 Vue.use(Plugin)
 
 new Vue({
@@ -33,7 +44,11 @@ new Vue({
         loading1: false,
         loading2: false,
         loading3: false,
-        message: 'hi'
+        message: 'hi',
+        selectedTab: {
+            type:String,
+            default: 'sports'
+        }
     },
     methods: {
         showToast1(){
---
+++ src/tabs-head.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="tabs-header">
+        <slot></slot>
+        <slot name="actions"></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'zooeyTabs-header'
+    }
+</script>
+<style lang="scss" scoped> 
+    .tabs-header{
+        
+    }
+</style>
