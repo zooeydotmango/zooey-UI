d35d4d39f14729145d7129bd132d3c1979b503b2
zooeydotmango <newzoe@qq.com>
2019-10-15 02:31:48 +0800

    创建popover组件，实现点击显示功能

---
+++ src/popover.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="popover" @click="xxx">
+        <div class="content-wrapper" v-if="visible">
+            <slot name="content" ></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "zooeyPopover",
+        data(){
+            return{
+                visible:false
+            }
+        },
+        methods:{
+            xxx(){
+                console.log('点击了popover');
+                this.visible=!this.visible; 
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .popover{
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
+        >.content-wrapper{
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            box-shadow:0 0 3px rgba(0,0,0,.55)
+        }
+    }
+    
+</style>
